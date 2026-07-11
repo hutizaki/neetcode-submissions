0a345dd29acd336d02daa7ea30bec9b416d66d27
Bryan Ho <bryan.v.ho-1@ou.edu>
2026-07-11 17:49:39 -0500

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for (char c : s) {
+            if(isalnum(c)) {
+                str += tolower(c);
+            }
+        }
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+
+        return str == rev;
+    }
+};
