c5bb7ff14b7cd17912774d24f0537db77bd322c6
Bryan Ho <bryan.v.ho-1@ou.edu>
2026-07-15 11:14:32 -0500

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            // 1. Skip outer duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int curr = nums[i] + nums[left] + nums[right];
+
+                if (curr > 0) {
+                    right--;
+                } else if (curr < 0) {
+                    left++;
+                } else {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    
+                    // 2. Skip inner duplicates
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
